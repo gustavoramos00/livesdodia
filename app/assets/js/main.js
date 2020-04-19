75e5d860bbcb2b3b6ff5359358c7427c9b37ab3b
Gustavo <gustavoramos00@gmail.com>
2020-04-19 12:22:41 -0300

    Substitui owl-carousel por tiny-slider
    WIP filtra/busca eventos

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    margin: 10,
-    autoWidth:true,
-    autoplay:false,
-    dots: true,
-    nav: false,
-    autoHeight:false,
-    center: true,
-    rewind: true,
+
+  // jplist.init();
+
+  var slider = tns({
+    container: '.client-slider',
+    controls: false,
+    items: 1,
+    mouseDrag: true,
+    loop: false,
+    responsive: {
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
   });
+
+
+  // //get a jPList control element
+  // const element = document.getElementById('jplist-control-element');
+
+  // //listen to the state event
+  // element.addEventListener('jplist.state', function(e) {
+
+  //   $('.dia-box:not(:has(.searchable))').hide();
+  //   $('.dia-box:has(.searchable)').show();
+  //   slider.goTo('first');
+
+  // });
 });
--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ name := """livesdodia"""
 organization := "br.com.livesdodia"
 maintainer := "gustavo@livesdodia.com.br"
 
-version := "0.8"
+version := "0.9"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
