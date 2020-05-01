017bb66a846140097ae6b006eb804f09148e199d
Gustavo <gustavoramos00@gmail.com>
2020-05-01 15:16:52 -0300

    Envia eventos toggle e video-play

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -24,51 +24,56 @@ window.onscroll = function() {
 } 
 
 
-  jplist.init();
+jplist.init();
 
 
-  $('.testi-meta-inner').on('click touch', function () {
-    var control = '#' + $(this).attr('collapse-control');
-    $(this).attr('aria-expanded',true);
-    $(control).collapse('toggle');
+$('.testi-meta-inner').on('click touch', function () {
+  var controlName = '#' + $(this).attr('collapse-control');
+  $(controlName).collapse('toggle');
+  ga('send', {
+    hitType: 'event',
+    eventCategory: 'Lives',
+    eventAction: 'toggle',
+    eventLabel: controlName
   });
+});
 
-  var slider = tns({
-    container: '.client-slider',
-    controls: false,
-    arrowKeys: true,
-    items: 1,
-    mouseDrag: true,
-    loop: false,
-    responsive: {
-      768: {
-        items: 2
-      },
-      992: {
-        items: 3
-      }
+var slider = tns({
+  container: '.client-slider',
+  controls: false,
+  arrowKeys: true,
+  items: 1,
+  mouseDrag: true,
+  loop: false,
+  responsive: {
+    768: {
+      items: 2
+    },
+    992: {
+      items: 3
     }
-  });
+  }
+});
 
 
-  //get a jPList control element
-  const element = document.getElementById('jplist-control-element');
+//get a jPList control element
+const element = document.getElementById('jplist-control-element');
 
-  //listen to the state event
-  element.addEventListener('jplist.state', function(e) {
+//listen to the state event
+element.addEventListener('jplist.state', function(e) {
 
-    /** 
-     * Oculta dias que não tem eventos satisfazendo os critérios de busca 
-    */
-    $('.dia-box:not(:has(.searchable))').hide();
-    $('.dia-box:has(.searchable)').show();
-    slider.goTo('first');
+  /** 
+   * Oculta dias que não tem eventos satisfazendo os critérios de busca 
+  */
+  $('.dia-box:not(:has(.searchable))').hide();
+  $('.dia-box:has(.searchable)').show();
+  slider.goTo('first');
 
-    /**
-     * Exibe 'Busca não encontrada' nas seções
-     */
-    $('.search-not-found-container:not(:has(.searchable))').find('.busca-nao-encontrada').show();
-    $('.search-not-found-container:has(.searchable)').find('.busca-nao-encontrada').hide();
+  /**
+   * Exibe 'Busca não encontrada' nas seções
+   */
+  $('.search-not-found-container:not(:has(.searchable))').find('.busca-nao-encontrada').show();
+  $('.search-not-found-container:has(.searchable)').find('.busca-nao-encontrada').hide();
 
-  });
+});
 });
--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ name := """livesdodia"""
 organization := "br.com.livesdodia"
 maintainer := "gustavo@livesdodia.com.br"
 
-version := "0.24"
+version := "0.25"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
